867581d6cd24fbb8453f251716654683f336e783
ranckprogram <1474574022@qq.com>
2024-04-08 12:27:32 +0800

    code

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const router = express.Router();
+
+const managerList = require("./manager-list");
+
+for (const name in managerList) {
+  router.get(`/${name}`, (req, res) => {
+    res.redirect(managerList[name]);
+  });
+}
+
+app.use("/", router);
+
+app.listen(1000, () => {
+  console.log("ok");
+});
---
+++ manager-list.js
@@ -0,0 +1,3 @@
+module.exports = {
+  user1: "https://work.weixin.qq.com/ca/cawcdeb56056c3ac53",
+};
